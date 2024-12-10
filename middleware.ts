03ed88473303682d2490a93681707cddd12ce9c2
cnann0615 <leave.it.to.7010@gmail.com>
2024-12-10 23:30:05 +0900

    1210 デプロイ時のESlintのエラーを修正

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,12 @@
-export { auth } from "./auth";
+import { auth } from "./auth";
+
+export default auth((req) => {
+  if (!req.auth && req.nextUrl.pathname !== "/login") {
+    const newUrl = new URL("/", req.nextUrl.origin);
+    return Response.redirect(newUrl);
+  }
+});
 
 export const config = {
-  matcher: ["/((?!^/auth).*)", "/((?!^/$).*)"],
+  matcher: ["/main/*", "/logManagement/*", "/wantedItem/*"],
 };
