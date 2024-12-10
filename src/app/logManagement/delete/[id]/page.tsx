c9c802b0d12f2173523e31624026dd236d7f10cb
cnann0615 <leave.it.to.7010@gmail.com>
2024-12-10 22:35:27 +0900

    1210 デプロイ時のESlintのエラーを修正

--- src/app/logManagement/delete/[id]/page.tsx
+++ src/app/logManagement/delete/[id]/page.tsx
@@ -3,14 +3,9 @@ import React from "react";
 import { prisma } from "../../../../../prisma";
 import DeleteLogForm from "@/app/logManagement/_components/DeleteLogForm";
 
-const page = async ({
-  params,
-}: {
-  params: {
-    id: string;
-  };
-}) => {
-  const id = params.id;
+const page = async ({ params }: { params: Promise<{ id: string }> }) => {
+  const { id } = await params;
+
   let log = null;
 
   try {
