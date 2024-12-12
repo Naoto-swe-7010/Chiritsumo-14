0f44a3a2001c9a76f5214dd5b9c01277f3ca938c
cnann0615 <leave.it.to.7010@gmail.com>
2024-12-12 21:52:23 +0900

    1212 Auth.jsの設定周りを変更

--- src/app/SignIn.tsx
+++ src/app/SignIn.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Link from "next/link";
 import { FcGoogle } from "react-icons/fc";
 import { TbMountain } from "react-icons/tb";
+import { signIn } from "../../auth";
 
 const SignIn = () => {
   return (
@@ -14,13 +15,18 @@ const SignIn = () => {
         <div className="inline-block">無駄な出費を抑えて、</div>
         <div className="inline-block">欲しいものを手に入れよう！</div>
       </h3>
-      <Link
-        href={"/api/auth/signin"}
-        className="flex w-[90%] max-w-md flex-nowrap items-center justify-center gap-3 rounded-full bg-white/10 px-6 py-3 text-base font-semibold no-underline transition hover:bg-white/20 sm:w-[70%] sm:text-lg md:w-[40%] md:text-xl lg:w-[25%]"
+      <form
+        action={async () => {
+          "use server";
+          await signIn("google");
+        }}
+        className="flex justify-center rounded-full bg-white/10 px-6 py-3 text-base font-semibold no-underline transition hover:bg-white/20 sm:w-[70%] sm:text-lg md:w-[40%] md:text-xl lg:w-[25%]"
       >
-        <FcGoogle className="size-6 sm:size-7 md:size-8" />
-        <p>Sign in with Google！</p>
-      </Link>
+        <button className="flex flex-nowrap justify-center items-center max-w-md gap-3 ">
+          <FcGoogle className="size-6 sm:size-7 md:size-8 " />
+          <p>Sign in with Google！</p>
+        </button>
+      </form>
     </div>
   );
 };
