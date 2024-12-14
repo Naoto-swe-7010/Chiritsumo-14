4047a77ab1aabb772e275e4cbd39134dba5a812a
cnann0615 <leave.it.to.7010@gmail.com>
2024-12-14 17:54:04 +0900

    1214 middleware.tsにmatcherを追加

--- auth.ts
+++ auth.ts
@@ -6,4 +6,5 @@ import { prisma } from "./prisma";
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [Google],
+  basePath: "/api/auth",
 });
