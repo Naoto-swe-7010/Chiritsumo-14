77897113687652455910c91bf89e579932a5e26a
cnann0615 <leave.it.to.7010@gmail.com>
2024-12-15 13:00:22 +0900

    1215 軽微な修正

--- src/app/logManagement/logTable/[page]/page.tsx
+++ src/app/logManagement/logTable/[page]/page.tsx
@@ -29,46 +29,48 @@ const page = async ({ params }: { params: Promise<{ page: string }> }) => {
     });
 
     return (
-      <>
+      <div>
         <div className="mb-20 mt-[64px] min-h-[calc(100vh-64px)] pb-10 pt-8">
           <div>
             <h1 className="mb-4 pl-1 text-xl font-bold text-gray-100 sm:text-2xl">
               ログ
             </h1>
             {logs && logs.length > 0 ? (
-              <table className="w-full border-collapse text-left text-sm">
-                <thead>
-                  <tr className="bg-black bg-opacity-50">
-                    {/* タイトル行の幅を広く */}
-                    <th className="w-3/5 p-2 font-semibold text-gray-200 sm:w-1/2">
-                      タイトル
-                    </th>
-                    <th className="w-1/6 p-2 font-semibold text-gray-200">
-                      値段
-                    </th>
-                    <th className="w-1/5 p-2 font-semibold text-gray-200">
-                      日時
-                    </th>
-                    <th className="w-1/6 p-2 font-semibold text-gray-200">
-                      アクション
-                    </th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <Suspense fallback={<RowSkeleton />}>
-                    {logs.map((log) => (
-                      <Row key={log.id} log={log} />
-                    ))}
-                  </Suspense>
-                </tbody>
-              </table>
+              <div>
+                <table className="w-full border-collapse text-left text-sm">
+                  <thead>
+                    <tr className="bg-black bg-opacity-50">
+                      {/* タイトル行の幅を広く */}
+                      <th className="w-3/5 p-2 font-semibold text-gray-200 sm:w-1/2">
+                        タイトル
+                      </th>
+                      <th className="w-1/6 p-2 font-semibold text-gray-200">
+                        値段
+                      </th>
+                      <th className="w-1/5 p-2 font-semibold text-gray-200">
+                        日時
+                      </th>
+                      <th className="w-1/6 p-2 font-semibold text-gray-200">
+                        アクション
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <Suspense fallback={<RowSkeleton />}>
+                      {logs.map((log) => (
+                        <Row key={log.id} log={log} />
+                      ))}
+                    </Suspense>
+                  </tbody>
+                </table>
+                <Pagination totalLogs={totalLogs} page={page} />
+              </div>
             ) : (
               <p className="text-center text-gray-500">ログがありません。</p>
             )}
           </div>
-          <Pagination totalLogs={totalLogs} page={page} />
         </div>
-      </>
+      </div>
     );
   } catch (error) {
     console.error("LogTableの取得中にエラーが発生しました:", error);
