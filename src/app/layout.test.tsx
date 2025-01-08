63b2455ccba4fe0d3e3d2673118c0a3f4b0e349d
cnann0615 <leave.it.to.7010@gmail.com>
2025-01-08 20:56:42 +0900

    0108 進捗率（率（%) が負の数にならないように修正

---
+++ src/app/layout.test.tsx
@@ -0,0 +1,58 @@
+import { render, screen } from '@testing-library/react'
+import RootLayout, { metadata } from './layout'
+
+// ヘッダーとフッターをモック
+vi.mock('./_components/_header/Header', () => ({
+  default: () => <header data-testid="header">Mock Header</header>,
+}))
+vi.mock('./_components/_footer/Footer', () => ({
+  default: () => <footer data-testid="footer">Mock Footer</footer>,
+}))
+
+describe('RootLayout Component', () => {
+  it('Header, Footer, Childrenのレンダリング確認', () => {
+    render(
+      <RootLayout>
+        <div data-testid="child">Test Content</div>
+      </RootLayout>,
+    )
+    // ヘッダーの存在確認
+    expect(screen.getByTestId('header')).toBeInTheDocument()
+    // 子要素のレンダリング確認
+    expect(screen.getByTestId('child')).toBeInTheDocument()
+    // フッターの存在確認
+    expect(screen.getByTestId('footer')).toBeInTheDocument()
+  })
+
+  it('メタデータの確認', () => {
+    expect(metadata.title).toBe('ちりつも')
+    expect(metadata.description).toBe(
+      '無駄な消費を我慢して欲しい物を手に入れよう！',
+    )
+    expect(metadata.openGraph).toEqual({
+      type: 'website',
+      locale: 'ja_JP',
+      url: 'https://chiritsumo-14.vercel.app',
+      siteName: 'ちりつも',
+      title: 'ちりつも',
+      description: '無駄な消費を我慢して欲しい物を手に入れよう！',
+      images: [
+        {
+          url: 'https://chiritsumo-14.vercel.app/opengraph-image.png',
+          width: 1200,
+          height: 630,
+          alt: 'ちりつも',
+        },
+      ],
+    })
+    expect(metadata.icons).toEqual({
+      icon: '/favicon.ico',
+      apple: '/apple-icon.ico',
+      other: {
+        rel: 'apple-touch-icon-precomposed',
+        url: '/apple-icon.ico',
+      },
+    })
+    expect(metadata.appleWebApp).toBe(true)
+  })
+})
--- src/app/main/_components/_BalanceProgress/ProgressBar.tsx
+++ src/app/main/_components/_BalanceProgress/ProgressBar.tsx
@@ -1,26 +1,29 @@
-import React from "react";
-import { Balance, WantedItem } from "@prisma/client";
+import React from 'react'
+import { Balance, WantedItem } from '@prisma/client'
 
 // 欲しいものリストの各アイテムの値段に対する残高の進捗をプログレスバーで可視化
 const ProgressBar = ({
   item,
   balance,
 }: {
-  item: WantedItem;
-  balance: Balance;
+  item: WantedItem
+  balance: Balance
 }) => {
   return (
     <div className="mt-2 flex items-center gap-2">
       <progress
         max="1"
         value={balance!.balance / item.price}
-        className="w-full"
-      ></progress>
+        className="w-full"></progress>
       <p className="text-sm sm:text-base">
-        {Math.min(Math.round((balance!.balance / item.price) * 100), 100)}%
+        {Math.max(
+          0,
+          Math.min(Math.round((balance!.balance / item.price) * 100), 100),
+        )}
+        %
       </p>
     </div>
-  );
-};
+  )
+}
 
-export default ProgressBar;
+export default ProgressBar
