7177e009eb9aa6b4292332a26fa59734bc71a97b
cnann0615 <leave.it.to.7010@gmail.com>
2025-01-09 21:20:55 +0900

    0109 e2e メインページのテスト修正

--- e2e/main.spec.ts
+++ e2e/main.spec.ts
@@ -11,90 +11,54 @@ import { describe } from 'node:test'
 //   },
 // })
 
-describe('メインページ', () => {
-  test('フォーム送信', async ({ browser }) => {
-    // Googole認証を突破するために、ブラウザにセッションのCookieを保存する
-    const context = await browser.newContext()
-    await context.addCookies([
-      {
-        name: 'authjs.session-token',
-        value: 'dummy',
-        domain: 'localhost:3000',
-        path: '/',
-      },
-    ])
-    // 残高をリセット
-    await prisma.balance.update({
+let context: any // 各テストで共有するブラウザコンテキスト
+let page: any // 各テストで共有するページ
+
+// DBの各テーブルをリセット
+const dbReset = async () => {
+  await prisma.$transaction([
+    prisma.balance.update({
       where: { userId: 'cm5nvvavn0000u5jqdeqvsi5l' },
       data: { balance: 0 },
-    })
-    // 欲しい物リストをリセット
-    await prisma.wantedItem.deleteMany({
+    }),
+    prisma.wantedItem.deleteMany({
       where: { userId: 'cm5nvvavn0000u5jqdeqvsi5l' },
-    })
-    // 欲しい物リストにアイテムを用意
-    await prisma.wantedItem.create({
-      data: {
-        createdAt: new Date(),
-        price: 20000,
-        name: 'Nintendo Switch',
-        url: 'https://example.com',
-        userId: 'cm5nvvavn0000u5jqdeqvsi5l',
-      },
-    })
-    // ログをリセット
-    await prisma.log.deleteMany({
+    }),
+    prisma.log.deleteMany({
       where: { userId: 'cm5nvvavn0000u5jqdeqvsi5l' },
-    })
+    }),
+  ])
+}
 
-    // ページ遷移
-    const page = await context.newPage()
-    await page.goto('http://localhost:3000/main')
+describe('メインページ', () => {
+  test.beforeEach(async ({ browser }) => {
+    // ブラウザ準備 ///////////////////////////////////////////////
 
-    // フォーム送信成功時
-    // 内容が残高に加算され、欲しい物アイテムの進捗率（％）に更新がかかる
-    await page.getByPlaceholder('我慢したものを入力').click()
-    await page.getByPlaceholder('我慢したものを入力').fill('飲み会')
-    await page.getByPlaceholder('節約できた額を入力').click()
-    await page.getByPlaceholder('節約できた額を入力').fill('5000')
-    await page.getByRole('button', { name: '我慢できた！' }).click()
-    await expect(page.getByText('5000')).toBeVisible()
-    await expect(page.getByText('25%')).toBeVisible()
-    // ログページにログが作成される
-    await page.goto('http://localhost:3000/logManagement/logTable/1')
-    await expect(page.getByText('飲み会')).toBeVisible()
-    await expect(page.getByText('5000')).toBeVisible()
-
-    // フォーム送信失敗時（空で送信された時）
-    // 残高と欲しい物アイテムの進捗率共に変更されない
-    await page.goto('http://localhost:3000/main')
-    await page.getByRole('button', { name: '我慢できた！' }).click()
-    await expect(page.getByText('5000')).toBeVisible()
-  })
-
-  test('欲しい物購入', async ({ browser }) => {
-    // Googole認証を突破するために、ブラウザにセッションのCookieを保存する
-    const context = await browser.newContext()
+    // 新しいブラウザコンテキストを作成
+    context = await browser.newContext()
+    // Google認証用のCookieを保存
     await context.addCookies([
       {
         name: 'authjs.session-token',
         value: 'dummy',
-        domain: 'localhost:3000',
+        domain: 'localhost',
         path: '/',
       },
     ])
-    // 残高をリセット
-    await prisma.balance.update({
-      where: { userId: 'cm5nvvavn0000u5jqdeqvsi5l' },
-      data: { balance: 50000 },
-    })
-    // 欲しい物リストをリセット
-    await prisma.wantedItem.deleteMany({
-      where: { userId: 'cm5nvvavn0000u5jqdeqvsi5l' },
-    })
+    // 新しいページを作成
+    page = await context.newPage()
+
+    // データ準備 ///////////////////////////////////////////////
+    // 残高：0円
+    // ログ: 0件
+    // 欲しい物リスト：１件（Nintendo Switch）
+
+    await dbReset()
+
     // 欲しい物リストにアイテムを用意
     await prisma.wantedItem.create({
       data: {
+        id: 'testId',
         createdAt: new Date(),
         price: 20000,
         name: 'Nintendo Switch',
@@ -102,29 +66,86 @@ describe('メインページ', () => {
         userId: 'cm5nvvavn0000u5jqdeqvsi5l',
       },
     })
-    // ログをリセット
-    await prisma.log.deleteMany({
-      where: { userId: 'cm5nvvavn0000u5jqdeqvsi5l' },
+    //////////////////////////////////////////////////////////////
+  })
+
+  test.afterEach(async () => {
+    // ブラウザコンテキストを閉じる
+    await context.close()
+
+    await dbReset()
+  })
+
+  describe('フォーム送信', () => {
+    test('フォーム送信成功時', async () => {
+      // ページ遷移
+      await page.goto('http://localhost:3000/main')
+      // フォーム入力(飲み会：5000円)
+      await page.getByPlaceholder('我慢したものを入力').click()
+      await page.getByPlaceholder('我慢したものを入力').fill('飲み会')
+      await page.getByPlaceholder('節約できた額を入力').click()
+      await page.getByPlaceholder('節約できた額を入力').fill('5000')
+      // 送信
+      await page.getByRole('button', { name: '我慢できた！' }).click()
+      // 残高が正しく増える（5000円）
+      await expect(
+        page.getByRole('heading', { name: 'balance' }),
+      ).toBeVisible()
+      // 欲しい物アイテムの進捗率が正しく増える（5000 / 20000 * 100)
+      await expect(page.getByText('25%')).toBeVisible()
+      // ログページにログが作成される
+      await page.goto('http://localhost:3000/logManagement/logTable/1')
+      await expect(page.getByText('飲み会')).toBeVisible()
+      await expect(page.getByText(5000)).toBeVisible()
     })
 
-    // ページ遷移
-    const page = await context.newPage()
-    await page.goto('http://localhost:3000/main')
-    // アイテム（Nintendo Switch）購入
-    await page.getByRole('button', { name: '購入' }).click()
-    // 購入ページにアイテム名（Nintendo Switch）の記載があるか
-    await expect(
-      page.getByRole('heading', { name: 'Nintendo Switch' }),
-    ).toBeVisible()
-    // 購入しますか？の問いに「はい」を選択
-    await page.getByRole('button', { name: 'はい' }).click()
-    // 購入完了ページに遷移するか
-    await expect(
-      page.getByRole('heading', { name: '残高を消費しました！' }),
-    ).toBeVisible()
-    // ホームに戻るボタンを押下
-    await page.getByRole('button', { name: 'ホームに戻る' }).click()
-    // 残高が元の50000から購入アイテム代20000を引いた、30000になっているか
-    await expect(page.getByText('30000')).toBeVisible()
+    test('フォーム送信失敗時（バリデーションエラー）', async () => {
+      // ページ遷移
+      await page.goto('http://localhost:3000/main')
+      // 空のままフォームを送信
+      await page.getByRole('button', { name: '我慢できた！' }).click()
+      // 残高は0円のまま
+      await expect(
+        page.getByRole('heading', { name: 'balance' }),
+      ).toHaveText('0')
+    })
+  })
+
+  describe('欲しい物購入', () => {
+    test.beforeEach(async () => {})
+
+    test('購入処理', async () => {
+      // 残高に50000追加
+      await prisma.balance.update({
+        where: { userId: 'cm5nvvavn0000u5jqdeqvsi5l' },
+        data: { balance: 50000 },
+      })
+      // ページ遷移
+      await page.goto('http://localhost:3000/main')
+      // アイテム（Nintendo Switch）購入ボタンを押下
+      await page.getByRole('button', { name: '購入' }).click()
+      // 購入確認ページに遷移したか
+      await expect(page).toHaveURL(
+        'http://localhost:3000/main/purchaseWantedItem/testId',
+      )
+      // 購入ページにアイテム名（Nintendo Switch）の記載があるか
+      await expect(
+        page.getByRole('heading', { name: 'Nintendo Switch' }),
+      ).toBeVisible()
+      // 購入しますか？の問いに「はい」を選択
+      await page.getByRole('button', { name: 'はい' }).click()
+      // 購入完了ページに遷移したか
+      await expect(page).toHaveURL(
+        'http://localhost:3000/main/purchaseWantedItem/postPurchase',
+      )
+      // ホームに戻るボタンを押下
+      await page.getByRole('button', { name: 'ホームに戻る' }).click()
+      // ホームに遷移したか
+      await page.goto('http://localhost:3000/main')
+      // 残高が元の50000から購入アイテム代20000を引いた、30000になっているか
+      await expect(
+        page.getByRole('heading', { name: 'balance' }),
+      ).toBeVisible()
+    })
   })
 })
--- src/app/main/_components/_BalanceDisplay/Balance.tsx
+++ src/app/main/_components/_BalanceDisplay/Balance.tsx
@@ -1,17 +1,19 @@
-import React from "react";
-import { getBalance, getSessionAndUserId } from "@/app/lib/commonFunction";
+import React from 'react'
+import { getBalance, getSessionAndUserId } from '@/app/lib/commonFunction'
 
 const Balance = async () => {
   try {
     // UserID取得
-    const userId = await getSessionAndUserId();
+    const userId = await getSessionAndUserId()
     // 残高取得
-    const balance = await getBalance(userId);
+    const balance = await getBalance(userId)
 
     // 取得した残高を表示
-    return <div>{balance.balance}</div>;
+    return <h1 aria-label="balance">{balance.balance}</h1>
   } catch {
-    console.error("Balanceテーブルの取得または作成中にエラーが発生しました:");
+    console.error(
+      'Balanceテーブルの取得または作成中にエラーが発生しました:',
+    )
   }
-};
-export default Balance;
+}
+export default Balance
