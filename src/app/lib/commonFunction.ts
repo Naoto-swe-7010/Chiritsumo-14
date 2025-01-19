bb53afca45275d3bf44eda379e292e8df936064a
cnann0615 <leave.it.to.7010@gmail.com>
2025-01-19 21:49:43 +0900

    0119 型定義にomitやpickを使用

--- src/app/lib/commonFunction.ts
+++ src/app/lib/commonFunction.ts
@@ -1,74 +1,74 @@
-import { cache } from "react";
+import { cache } from 'react'
 
-import { auth } from "../../../auth";
-import { prisma } from "../../../prisma";
+import { auth } from '../../../auth'
+import { prisma } from '../../../prisma'
 
 // 日付フォーマット関数
 export const formattedDate = (date: Date): string => {
   return date
-    .toLocaleString("ja-JP", {
-      year: "2-digit",
-      month: "2-digit",
-      day: "2-digit",
-      hour: "2-digit",
-      minute: "2-digit",
+    .toLocaleString('ja-JP', {
+      year: '2-digit',
+      month: '2-digit',
+      day: '2-digit',
+      hour: '2-digit',
+      minute: '2-digit',
     })
-    .replace(/^20/, "");
-};
+    .replace(/^20/, '')
+}
 
 // データ取得（全てユーザに紐づくデータ取得のためDynamic Rendering）///////////////////////////////////
 
 // Sessionの取得
 export const getSession = cache(async () => {
-  return auth();
-});
+  return auth()
+})
 
 // UserIDの取得
 export const getSessionAndUserId = cache(async () => {
   try {
-    const session = await getSession();
+    const session = await getSession()
     if (!session || !session.user || !session.user.id) {
-      throw new Error("セッション情報が取得できませんでした。");
+      throw new Error('セッション情報が取得できませんでした。')
     }
-    return session.user.id;
+    return session.user.id
   } catch (error) {
-    console.error(error);
-    throw new Error("認証が必要です。");
+    console.error(error)
+    throw new Error('認証が必要です。')
   }
-});
+})
 
 // Balanceレコードの取得(存在しない場合は新規作成)
 export const getBalance = cache(async (userId: string) => {
   let balance = await prisma.balance.findUnique({
     where: { userId },
-  });
+  })
   if (!balance) {
     balance = await prisma.balance.create({
       data: {
         userId,
         balance: 0,
       },
-    });
+    })
   }
-  return balance;
-});
+  return balance
+})
 
 // ログの取得
 export const getLog = cache(
   async (userId: string, pageNumber: number, pageSize: number) => {
     return prisma.log.findMany({
       where: { userId },
-      orderBy: { createdAt: "desc" },
+      orderBy: { createdAt: 'desc' },
       skip: (pageNumber - 1) * pageSize,
       take: pageSize,
-    });
-  }
-);
+    })
+  },
+)
 
 // 欲しいものリストの取得
 export const getWantedItemList = cache(async (userId: string) => {
   return prisma.wantedItem.findMany({
     where: { userId },
-    orderBy: { createdAt: "desc" },
-  });
-});
+    orderBy: { createdAt: 'desc' },
+  })
+})
--- src/app/lib/formState.ts
+++ src/app/lib/formState.ts
@@ -1,36 +1,25 @@
-// エラーメッセージ格納用の型定義
+import { Log, WantedItem } from '@prisma/client'
+
+// 共通のエラーメッセージ型
+type ErrorMessages<T> = Partial<Record<keyof T, string[]>>
+
+// 各フォームステートの型定義
 export type AddBalanceFormState = {
-  errors?: {
-    title?: string[];
-    price?: string[];
-  };
-  message?: string | null;
-};
+  errors?: ErrorMessages<Pick<Log, 'title' | 'price'>>
+  message?: string | null
+}
 
 export type UpdateLogFormState = {
-  errors?: {
-    id?: string[];
-    title?: string[];
-    price?: string[];
-  };
-  message?: string | null;
-};
+  errors?: ErrorMessages<Pick<Log, 'id' | 'title' | 'price'>>
+  message?: string | null
+}
 
 export type AddWantedItemFormState = {
-  errors?: {
-    name?: string[];
-    price?: string[];
-    url?: string[];
-  };
-  message?: string | null;
-};
+  errors?: ErrorMessages<Pick<WantedItem, 'name' | 'price' | 'url'>>
+  message?: string | null
+}
 
 export type UpdateWantedItemFormState = {
-  errors?: {
-    id?: string[];
-    name?: string[];
-    price?: string[];
-    url?: string[];
-  };
-  message?: string | null;
-};
+  errors?: ErrorMessages<Pick<WantedItem, 'id' | 'name' | 'price' | 'url'>>
+  message?: string | null
+}
