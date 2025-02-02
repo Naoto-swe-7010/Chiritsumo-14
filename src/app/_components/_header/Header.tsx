a926c02daeb0a3f3916b4e56093a5239e85e5d86
cnann0615 <leave.it.to.7010@gmail.com>
2025-02-02 13:41:28 +0900

    0202 header, footerの色変更

--- src/app/_components/_header/Header.tsx
+++ src/app/_components/_header/Header.tsx
@@ -1,16 +1,15 @@
-import React from "react";
+import React from 'react'
 
-import AccountInfo from "./AccountInfo";
-import { getSession } from "@/app/lib/commonFunction";
-import Title from "./Title";
-import Menu from "./Menu";
-
+import AccountInfo from './AccountInfo'
+import { getSession } from '@/app/lib/commonFunction'
+import Title from './Title'
+import Menu from './Menu'
 
 const Header = async () => {
-  const session = await getSession();
+  const session = await getSession()
 
   return (
-    <header className="fixed left-0 top-0 z-10 w-full bg-gray-800 px-[4%] text-white">
+    <header className="fixed left-0 top-0 z-10 w-full bg-gray-900 px-[4%] text-white">
       <nav className="mx-auto flex h-16 items-center justify-between px-4">
         <div className="flex items-center gap-6">
           <Title />
@@ -22,7 +21,7 @@ const Header = async () => {
         {session && <AccountInfo session={session} />}
       </nav>
     </header>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
