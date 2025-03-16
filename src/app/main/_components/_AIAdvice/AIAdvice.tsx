a1fc6f69b2d39efb2ef1bb176a2f652e64cf17a7
cnann0615 <leave.it.to.7010@gmail.com>
2025-03-16 23:25:02 +0900

    fix  AI機能のレスポンシブ対応を修正

--- src/app/main/_components/_AIAdvice/AIAdvice.tsx
+++ src/app/main/_components/_AIAdvice/AIAdvice.tsx
@@ -15,30 +15,30 @@ const AIAdvice = () => {
     <div className="mt-10 flex justify-center p-4">
       <div className="w-full max-w-lg space-y-6">
         {/* 入力フォーム */}
-        <div className="overflow-hidden rounded-lg bg-gradient-to-r from-purple-500/10 to-pink-500/10 p-8 shadow-lg">
+        <div className="overflow-hidden rounded-lg bg-gradient-to-r from-purple-500/10 to-pink-500/10 py-8 px-4 sm:px-8 shadow-lg">
           {/* タイトル部分 */}
           <div className="mb-6 flex items-center justify-center space-x-2">
-            <WiStars className="h-8 w-8 text-cyan-400" />
-            <h2 className="bg-gradient-to-r from-cyan-400 via-purple-400 to-pink-400 bg-clip-text text-xl font-bold text-transparent">
+            <WiStars className="h-6 w-6 sm:h-8 sm:w-8 text-cyan-400" />
+            <h2 className="bg-gradient-to-r from-cyan-400 via-purple-400 to-pink-400 bg-clip-text text-lg sm:text-xl font-bold text-transparent">
               AI 節約アドバイス
             </h2>
-            <WiStars className="h-8 w-8 text-pink-400" />
+            <WiStars className="h-6 w-6 sm:h-8 sm:w-8 text-pink-400" />
           </div>
           <form
             action={formAction}
             name="keyword"
-            className="flex flex-col sm:flex-row items-center gap-4 sm:gap-6">
+            className="flex flex-row items-center gap-2 sm:gap-4">
             <input
               type="text"
               name="keyword"
               placeholder="キーワードを入力"
-              className="flex-1 rounded-lg bg-gray-800/50 px-4 py-3 text-white placeholder-gray-400 shadow-inner transition-all focus:bg-gray-800/70 focus:outline-none focus:ring-2 focus:ring-purple-400 border-2 border-cyan-400 border-purple-400 border-pink-400"
+              className="flex-1 rounded-lg bg-gray-800/50 px-2 py-2 text-white placeholder-gray-400 shadow-inner transition-all focus:bg-gray-800/70 focus:outline-none focus:ring-2 focus:ring-purple-400 border-2 border-cyan-400 border-purple-400 border-pink-400"
             />
             <Button
-              className="rounded-lg bg-gradient-to-r from-purple-500 to-pink-500 px-6 py-3 h-[50px] font-bold text-white shadow-md transition-all hover:opacity-90 hover:shadow-lg active:scale-95 "
+              className="rounded-lg bg-gradient-to-r from-purple-500 to-pink-500 px-4 py-2 h-[40px] font-bold text-white shadow-md transition-all hover:opacity-90 hover:shadow-lg active:scale-95 "
               disabled={isPending}>
               {isPending ?
-                <LoadingSpinner size={24} color="white" />
+                <LoadingSpinner size={20} color="white" />
               : '送信'}
             </Button>
           </form>
