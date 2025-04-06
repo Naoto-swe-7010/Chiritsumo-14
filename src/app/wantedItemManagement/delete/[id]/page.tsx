15a6d7bfbf3ad9656b570197a489be1e036657d5
cnann0615 <leave.it.to.7010@gmail.com>
2025-04-06 15:35:01 +0900

    fix: #58 モーダル表示時のバックグラウンドの高さが足りていないバグを解消
    モーダル時に、pageの高さ（min）が設定されていなく、途中で途切れていたため、モーダル使用ページ全てに高さ（min）を設定した。

--- src/app/wantedItemManagement/delete/[id]/page.tsx
+++ src/app/wantedItemManagement/delete/[id]/page.tsx
@@ -30,10 +30,12 @@ const page = async ({ params }: { params: { id: string } }) => {
   }
 
   return (
-    <Modal>
-      <h2 className="mb-4 text-lg font-bold">削除</h2>
-      <DeleteWantedItemForm item={item} />
-    </Modal>
+    <div className="mb-20 mt-[64px] min-h-[calc(100vh-64px)]">
+      <Modal>
+        <h2 className="mb-4 text-lg font-bold">削除 </h2>
+        <DeleteWantedItemForm item={item} />
+      </Modal>
+    </div>
   );
 };
 
